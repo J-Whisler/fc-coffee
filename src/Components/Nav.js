16fa0c848e4880c6a88cd5442a14a35990bc9295
J-Whisler <jwhisler521@gmail.com>
2021-04-20 18:09:36 -0400

    Components folder and Nav.js/scss files

---
+++ src/Components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import "./Nav.scss";
+
+const Nav = () => {
+  return(
+    <h1 className="nav">Nav</h1>
+  )
+  
+};
+
+export default Nav;
