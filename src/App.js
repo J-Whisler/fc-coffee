ddcd484603d16cabff64e58de361158f1a64e94e
J-Whisler <jwhisler521@gmail.com>
2021-04-25 18:06:28 -0400

    setup Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 
+import {BrowserRouter as Router} from 'react-router-dom'
+
 import Nav from "./Components/Nav";
 
 const App = () => {
   return (
     <>
-      <h1>App</h1>
-      <Nav />
+      <Router>
+        <Nav />
+      </Router>
     </>
   );
 };
