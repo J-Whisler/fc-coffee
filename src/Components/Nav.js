00ba247d55ca30e675f89f1aa03cd7cd1fff835d
J-Whisler <jwhisler521@gmail.com>
2021-04-25 18:07:05 -0400

    nav content and styling updates

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,12 +1,42 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Nav.scss";
 
 const Nav = () => {
-  return(
-    <h1 className="nav">Nav</h1>
-  )
-  
+  return (
+    <>
+      <nav className="nav">
+        <div className="nav-container">
+          <Link to="/" className="nav-logo">
+            <div className="logo-top">
+              F.C. <i className="fas fa-mug-hot"></i>
+            </div>
+            <div className="logo-bottom">Coffee</div>
+          </Link>
+          <div className="menu-icon">
+            <i className="fas fa-bars"></i>
+          </div>
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <Link to="/" className="nav-links">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links">
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links">
+                Products
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Nav;
