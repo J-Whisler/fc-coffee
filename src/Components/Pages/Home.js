fea7439d485afc7b894406b702152331f24f4e0b
J-Whisler <jwhisler521@gmail.com>
2021-04-28 18:46:31 -0400

    created pages folder and Home.js file to use for homepage

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../../App.css";
+import Showcase from '../Showcase';
+
+
+const Home = () => {
+    return(
+        <>
+        <Showcase />
+        </>
+    )
+}
+
+export default Home;
